3b49067ca0ee5be5667ecfad9f6a4041a2c7d036
LelinZheng <lezi.lelin@gmail.com>
2025-07-29 00:26:59 -0700

    implement circuit breaker for api routes

--- product-service/src/main/java/com/techie/microservices/product/controller/ProductController.java
+++ product-service/src/main/java/com/techie/microservices/product/controller/ProductController.java
@@ -28,6 +28,12 @@ public class ProductController {
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
   public List<ProductResponse> getAllProducts(){
+//    try{
+//      Thread.sleep(50000);
+//    }catch(InterruptedException e){
+//      throw new RuntimeException(e);
+//    }
+
     return productService.getAllProducts();
   }
 
